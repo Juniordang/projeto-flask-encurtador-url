d7d37b0a23961e5cf3f8d4a9d9748015b41d2d6b
junin-dev <juniordange4@gmail.com>
2026-03-24 18:09:45 -0300

    fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Configuração do Banco de Dados SQLite (será criado um arquivo 'database.db')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
